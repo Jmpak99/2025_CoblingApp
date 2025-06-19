7d5ecc878ece1c7833d41334f726df51b437f202
Jmpak99 <jmpak99@gmail.com>
2025-06-20 04:34:30 +0900

    Initial Commit

---
+++ Cobling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cobling
+//
+//  Created by 박종민 on 6/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoblingTests/CoblingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoblingTests.swift
+//  CoblingTests
+//
+//  Created by 박종민 on 6/20/25.
+//
+
+import Testing
+@testable import Cobling
+
+struct CoblingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
