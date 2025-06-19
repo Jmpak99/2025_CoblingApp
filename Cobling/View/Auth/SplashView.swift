7f2881974e990456a61787c7e55ef2bf30199743
Jmpak99 <jmpak99@gmail.com>
2025-06-20 06:14:46 +0900

    ✨ Feat: SplashView 임시 화면 추가

---
+++ Cobling/View/Auth/SplashView.swift
@@ -0,0 +1,14 @@
+//
+//  SplashView.swift
+//  Cobling
+//
+//  Created by 박종민 on 6/20/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Splash View") // 임시 뷰 내용
+    }
+}
