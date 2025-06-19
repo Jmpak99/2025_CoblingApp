73d5ec645a473c54c185ed6b70231e3888cff68f
Jmpak99 <jmpak99@gmail.com>
2025-06-20 06:09:38 +0900

    ✨ Feat: Fonts.swift 생성 및 Pretendard, Gmarket, LeeSeoyun 커스텀 폰트 스타일 적용

---
+++ Cobling/Resources/Fonts.swift
@@ -0,0 +1,48 @@
+//
+//  Fonts.swift
+//  Cobling
+//
+//  Created by 박종민 on 6/20/25.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // Pretendard-Bold
+    static let pretendardBold28: Font = .custom("Pretendard-Bold", size: 28)
+    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
+    static let pretendardBold34: Font = .custom("Pretendard-Bold", size: 34)
+    
+    // Pretendard-SemiBold
+    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let pretendardSemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
+    
+    // Pretendard-Medium
+    static let pretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
+    static let pretendardMedium18: Font = .custom("Pretendard-Medium", size: 18)
+    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    
+    // Pretendard-Regular
+    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    static let pretendardRegular18: Font = .custom("Pretendard-Regular", size: 18)
+    
+    // Gmarket-Medium
+    static let gmarketMedium14: Font = .custom("GmarketSansMedium", size: 14)
+    static let gmarketMedium16: Font = .custom("GmarketSansMedium", size: 16)
+    static let gmarketMedium18: Font = .custom("GmarketSansMedium", size: 18)
+    static let gmarketMedium20: Font = .custom("GmarketSansMedium", size: 20)
+    static let gmarketMedium22: Font = .custom("GmarketSansMedium", size: 22)
+    
+    // Gmarket-Bold
+    
+    
+    // LeeSeoyun
+    static let leeseoyun48 : Font = .custom("LeeSeoyun", size: 48)
+    static let leeseoyun24 : Font = .custom("LeeSeoyun", size: 24)
+    
+    
+}
