8fece0aaa9e780c57a3bb0806dbecf2a7f0c4d53
Jmpak99 <jmpak99@gmail.com>
2025-06-24 15:40:59 +0900

    ✨ feat: 프로필 화면(Settings View) 기본 구조 추가

--- Cobling/View/Settings/SettingsView.swift
+++ Cobling/View/Settings/SettingsView.swift
@@ -5,3 +5,13 @@
 //  Created by 박종민 on 6/20/25.
 //
 
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+    }
+}
