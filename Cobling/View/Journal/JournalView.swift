d0ca0b607502feb6db747cfbb4fd95a3bcbdaf72
Jmpak99 <jmpak99@gmail.com>
2025-06-24 15:39:10 +0900

    ✨ feat: 기록장 화면(Journal View) 기본 구조 추가

--- Cobling/View/Journal/JournalView.swift
+++ Cobling/View/Journal/JournalView.swift
@@ -5,3 +5,13 @@
 //  Created by 박종민 on 6/20/25.
 //
 
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text("Journal View")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+    }
+}
