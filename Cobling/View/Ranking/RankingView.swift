126d48e12d67bcc7aa7135e8eeddd15cc1ff25d2
Jmpak99 <jmpak99@gmail.com>
2025-06-24 15:38:47 +0900

    ✨ feat: 랭킹 화면(RankingView) 기본 구조 추가

--- Cobling/View/Ranking/RankingView.swift
+++ Cobling/View/Ranking/RankingView.swift
@@ -5,3 +5,13 @@
 //  Created by 박종민 on 6/20/25.
 //
 
+import SwiftUI
+
+struct RankingView: View {
+    var body: some View {
+        Text("RankingView")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+    }
+}
