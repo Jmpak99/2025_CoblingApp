77920450551191bfe686b6a17574c08ed030b3d8
Jmpak99 <jmpak99@gmail.com>
2025-07-29 00:54:40 +0900

    💄 style: BlockPaletteView 배경 깜빡임 방지를 위한 레이아웃 안정화

--- Cobling/View/Quest/BlockPaletteView.swift
+++ Cobling/View/Quest/BlockPaletteView.swift
@@ -3,48 +3,51 @@ import SwiftUI
 // BlockPaletteView.swift
 struct BlockPaletteView: View {
     @EnvironmentObject var dragManager: DragManager
-
+    
     private let blockTypes: [BlockType] = [
         .moveForward, .turnLeft, .turnRight
     ]
-
+    
     var body: some View {
-        VStack(spacing: 12) {
-            ForEach(blockTypes, id: \.self) { type in
-                GeometryReader { geometry in
-                    Image(type.imageName)
-                        .resizable()
-                        .frame(width: 120, height: 30)
-                        .gesture(
-                            DragGesture(minimumDistance: 0)
-                                .onChanged { value in
-                                    let globalFrame = geometry.frame(in: .named("global"))
-                                    let globalPoint = CGPoint(
-                                        x: globalFrame.origin.x + value.location.x,
-                                        y: globalFrame.origin.y + value.location.y
-                                    )
-                                    let offset = CGSize(
-                                        width: value.startLocation.x - 80,
-                                        height: value.startLocation.y - 20
-                                    )
-                                    dragManager.prepareDragging(type: type, at: globalPoint, offset: offset)
-                                    dragManager.updateDragPosition(globalPoint)
-                                    dragManager.startDragging()
-                                }
-                                .onEnded { value in
-                                    let globalFrame = geometry.frame(in: .named("global"))
-                                    let endPoint = CGPoint(
-                                        x: globalFrame.origin.x + value.location.x,
-                                        y: globalFrame.origin.y + value.location.y
-                                    )
-                                    dragManager.endDragging(at: endPoint)
-                                }
-                        )
+        ZStack {
+            Color.white // 전체 배경 보장
+            VStack(spacing: 12) {
+                ForEach(blockTypes, id: \.self) { type in
+                    GeometryReader { geometry in
+                        Image(type.imageName)
+                            .resizable()
+                            .frame(width: 120, height: 30)
+                            .gesture(
+                                DragGesture(minimumDistance: 0)
+                                    .onChanged { value in
+                                        let globalFrame = geometry.frame(in: .named("global"))
+                                        let globalPoint = CGPoint(
+                                            x: globalFrame.origin.x + value.location.x,
+                                            y: globalFrame.origin.y + value.location.y
+                                        )
+                                        let offset = CGSize(
+                                            width: value.startLocation.x - 80,
+                                            height: value.startLocation.y - 20
+                                        )
+                                        dragManager.prepareDragging(type: type, at: globalPoint, offset: offset)
+                                        dragManager.updateDragPosition(globalPoint)
+                                        dragManager.startDragging()
+                                    }
+                                    .onEnded { value in
+                                        let globalFrame = geometry.frame(in: .named("global"))
+                                        let endPoint = CGPoint(
+                                            x: globalFrame.origin.x + value.location.x,
+                                            y: globalFrame.origin.y + value.location.y
+                                        )
+                                        dragManager.endDragging(at: endPoint)
+                                    }
+                            )
+                    }
+                    .frame(height: 40)
                 }
-                .frame(height: 40)
             }
+            .padding()
         }
-        .padding()
-        .background(Color.white)
     }
+    
 }
